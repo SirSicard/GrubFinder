8dbf29ac86bd11de3d166d1b3fb345828ed564d7
Rick <rickoliwer@gmail.com>
2021-03-24 15:53:37 +0100

    Add Fonts
    Added google fonts: Poppin & Merriweather

--- public/admin-form.php
+++ public/admin-form.php
@@ -11,31 +11,31 @@
 
         <div class="bg-white p-16 rounded-2xl shadow-md hover:shadow-lg w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
 
-            <h3 class="text-3xl font-semibold mb-10 texte-gray-700">Apply for Admin</h3>
+            <h3 class="text-3xl font-semibold mb-10 texte-gray-700 font-poppins">Apply for Admin</h3>
 
             <form action="" class="space-y-5">
 
                 <div>
-                    <label for="name" class="lable">Name</label>
+                    <label for="name" class="lable font-poppins">Name</label>
                     <input type="text" id="name" class="input">
                 </div>
 
                 <div>
-                    <label for="email" class="lable">Email</label>
+                    <label for="email" class="lable font-poppins">Email</label>
                     <input type="email" id="email" class="input">
                 </div>
 
                 <div>
-                    <label for="password" class="lable">Password</label>
+                    <label for="password" class="lable font-poppins">Password</label>
                     <input type="password" id="password" class="input">
                 </div>
                 
                 <div>
                     <input type="checkbox" id="checkbox">
-                    <label for="checkbox" class="ml-2 text-gray-500 text-sm">I agree to the terms and privacy</label>
+                    <label for="checkbox" class="ml-2 text-gray-500 text-sm font-merri">I agree to the terms and privacy</label>
                 </div>
 
-                <input type="submit" value="Apply" class="w-full button">
+                <input type="submit" value="Apply" class="w-full button font-poppins">
 
             </form>
 
